0905c9b0cd0d986acff2cb86cbdf23163e4ab4ba
Mário Ribeiro <ribeiromario249@gmail.com>
2023-02-19 13:33:12 +0000

    Gralheira-site

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+function Paginainicial() {
+  return (
+    <div>
+      <Head>
+        <title>Pagina Inicial</title>
+      </Head>
+      <h1 className='titulo'>Conteúdo da Minha Página</h1>
+    </div>
+  );
+}
+
+export default Paginainicial ;
