e9435cfa5cf333a7791d8bcd226cb273a950fa3e
Mário <ribeiromario249@gmail.com>
2023-02-24 23:17:41 +0000

    gralheira-site

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
-import styles from '../pages/index.module.css'
+import styles from '../pages/index.module.css';
 
 const Home = () => {
   return (
-    <div> 
-          <h1 className={styles.bigtitle}>Explore os Encantos Naturais da Gralheira</h1> 
-          <div className={styles.container}>
-              <button className={styles.button1}>Saber Mais</button>
-          </div>
+    <div className={styles.container}>
+      <div className={styles.overlay}>
+        <video className={styles.video} autoPlay muted loop>
+          <source src="videogralheira.mp4" type="video/mp4" />
+        </video>
+      </div>
+      <h1 className={styles.bigtitle}>Explore os Encantos Naturais da Gralheira</h1>
+      <div className={styles.buttoncontainer}>
+        <button className={styles.button1}>Saber Mais</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
